a461ed7c4da7b1f2c9960e25a822bd6c87f8ce07
AsterRisk <31030090+AsterRisk@users.noreply.github.com>
2020-05-02 23:51:53 -0500

    Updated table list in sql generation script

--- SQL Setup/prep.sql
+++ SQL Setup/prep.sql
@@ -2,6 +2,15 @@ drop database if exists final_proj_3161;
 create database final_proj_3161;
 use final_proj_3161;
 
+drop table if exists users;
+drop table if exists logins;
+drop table if exists friends;
+drop table if exists posts;
+drop table if exists media;
+drop table if exists comments;
+drop table if exists groups;
+drop table if exists members;
+
 create table users
 (
     user_id int not null unique,
@@ -19,11 +28,11 @@ create table users
 create table logins
 (
     user_id int not null unique,
-    email varchar (45) not null, unique,
+    email varchar (45) not null unique,
     pass_digest varchar(65) not null, 
     salt int not null, 
-    primary key (user_id, emails)
-)
+    primary key (user_id, email)
+);
 
 /*create table addresses
 (
@@ -38,19 +47,52 @@ create table logins
 
 create table friends
 (
-    friend_id int not null UNIQUE,
-    user_id int not null UNIQUE,
+    friend_id int not null,
+    user_id int not null,
     fgroup varchar(10),
-    foreign key(user_id) references users,
+    /*foreign key(user_id) references users,*/
     primary key (user_id, friend_id)
 );
 
 create table posts
 (
     post_id int not null UNIQUE,
-    user_id int not null UNIQUE,
+    user_id int not null,
     text_content varchar(300),
     media_link varchar(30),
-    foreign key (user_id) references users,
-    primary key (post_id, user_id)
-)
+    /*foreign key (user_id) references users,*/
+    primary key (post_id)
+);
+
+
+create table media
+(
+    user_id int not null,
+    media_link varchar(30) not null unique,
+    primary key (media_link)
+);
+
+create table comments
+(
+    comment_id int not null UNIQUE,
+    user_id int not null,
+    post_id int not null,
+    text_content varchar (300),
+    media_link varchar(30),
+    primary key (comment_id)
+);
+
+create table groups
+(
+    group_id int not null UNIQUE,
+    owner_id int not null,
+    group_name varchar(30) not null,
+    primary key (group_id, owner_id)
+);
+
+create table members
+(
+    group_id int not null,
+    member_id int not null,
+    primary key (group_id, member_id)
+);
